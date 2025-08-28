29041d5d48be8d68e33ecd4d47decf96cbf531af
AlexSantosLeite <alex.s.leite@hotmail.com>
2025-08-27 21:29:45 -0300

    feat: Adiciona menu hambúrguer responsivo

--- script.js
+++ script.js
@@ -135,3 +135,26 @@ document.addEventListener('DOMContentLoaded', function() {
 
     moveToSlide(0);
 });
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    if (burger && nav) {
+        burger.addEventListener('click', () => {
+            nav.classList.toggle('nav-active');
+
+            navLinks.forEach((link, index) => {
+                if (link.style.animation) {
+                    link.style.animation = '';
+                } else {
+                    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+                }
+            });
+
+            burger.classList.toggle('toggle');
+        });
+    }
+}
+navSlide();
